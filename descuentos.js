61b8e1de91b47e5993a9a54eace28d1b2700daa6
Sebastian Sanhueza <sebastian.sanhueza.t@usach.cl>
2021-08-23 00:22:05 -0400

    Desafio del cupon

--- descuentos.js
+++ descuentos.js
@@ -17,3 +17,20 @@ function ButPriceDiscount() {
     const ResulP = document.getElementById("ResulP");
     ResulP.innerText = "El precio con descuento es: $" + precioCDescuento;
 }
+
+function ButCupon() {
+    const inputPrice = document.getElementById("InputCupon");
+    const Cupon = inputPrice.value;
+
+    const Resultado = document.getElementById("DescuentoAplicado");
+
+    //Cupones validos
+    var cupones = ["holi", "Sipo niña", "Mi-Primera-Vez", "compre", "aqui", "Yo-Retiro"];
+    var descuentos = [15, 16, 17, 18, 19, 20];
+
+    if (cupones.includes(Cupon)) {
+        Resultado.innerText = "Tu descuento es del " + descuentos[cupones.indexOf(Cupon)] + "%";
+    } else {
+        Resultado.innerText = "Ingrese un cupón valido";
+    }
+}
